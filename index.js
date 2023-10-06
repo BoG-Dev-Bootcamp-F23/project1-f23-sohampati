312c359e7774631233c66edf145847c46402746c
Soham Pati <sohampati2005@gmail.com>
2023-10-05 23:17:16 -0400

    finished with back and forths now onto types

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
 const left_arrow = document.getElementsByClassName('arrow-button-left')[0];
 const rightarrow = document.getElementsByClassName('arrow-button-right')[0];
-const info_button = document.getElementsByClassName('inf');
-const moves_button = document.getElementsByClassName('moves');
-const info_or_moves = document.getElementsByClassName('info');
+const info_button = document.getElementById('infobutton');
+const moves_button = document.getElementById('movebutton');
+const info_or_moves = document.getElementsByClassName('info')[0];
 const poke_type = document.getElementsByClassName('type');
+const stats = document.getElementsByClassName('stats')[0];
 const img = document.getElementById('pic');
 const pokemon_name = document.getElementsByClassName('pokeType')[0];
 const height = document.getElementsByClassName('height')[0];
@@ -15,7 +17,112 @@ const special_att = document.getElementsByClassName('special-attack')[0];
 const special_def = document.getElementsByClassName('special-defense')[0];
 const speed = document.getElementsByClassName('speed')[0];
 
-let current = 300;
+let current = 132;
+let infoMode = true;
+
+
+
+moves_button.addEventListener("click", function () {
+    infoMode = false; // Update the value of infoMode
+    moves_button.style.backgroundColor = "#7CFF79"; 
+    info_button.style.backgroundColor = "#E8E8E8"
+    info_or_moves.textContent = "Moves"// Change the button color
+    let count = 0;
+    let x = fetchPoke(current);
+    stats.innerHTML = '';
+  
+
+    const furl = `https://pokeapi.co/api/v2/pokemon/${current}/`;
+    fetch(furl)
+      .then((response) => {
+          if(!response.ok) {
+            throw new Error(`Failed to fetch data for ${data.forms.name}`);
+          }
+          return response.json();
+        })
+      .then((data) => {
+          stats.innerHTML = '';
+          let y =0;
+          for(let i = 0; i < data.moves.length; i++){
+              stats.innerHTML += data.moves[i].move.name;
+              stats.innerHTML += "<br />"
+
+              y++;
+              
+          }
+              
+              
+console.log(y);
+
+
+
+          }
+          
+          )
+
+      
+    
+
+
+
+  })
+
+  info_button.addEventListener("click", function () {
+    infoMode = true; // Update the value of infoMode
+    info_button.style.backgroundColor = "#7CFF79"; 
+    moves_button.style.backgroundColor = "#E8E8E8"// Change the button color
+    info_or_moves.textContent = "Info"
+    stats.innerHTML = ' ';
+    const furl = `https://pokeapi.co/api/v2/pokemon/${current}/`;
+    fetch(furl)
+      .then((response) => {
+          if(!response.ok) {
+            throw new Error(`Failed to fetch data for ${data.forms.name}`);
+          }
+          return response.json();
+        })
+      .then((data) => {
+          stats.innerHTML = '';
+        //   data.height /= 10
+        //   let finalheight = checkAndAddDecimalZero(data.height);
+        //   height.textContent = "height: " + finalheight + "m";
+        //   data.weight = data.weight / 10;
+        //   let finalweight = checkAndAddDecimalZero(data.weight);
+        //   weight.textContent = "weight: " + finalweight + "kg";
+        //   hp.textContent = "hp: " + data.stats[0].base_stat;
+        //   attack.textContent = "attack: " + data.stats[1].base_stat;
+        //   defense.textContent = "defense: " + data.stats[2].base_stat;
+        //   special_att.textContent = "special-attack: " + data.stats[3].base_stat;
+        //   special_def.textContent = "special-defense: " + data.stats[4].base_stat;
+        //   speed.textContent = "speed: " + data.stats[5].base_stat;
+        data.height /= 10
+          let finalheight = checkAndAddDecimalZero(data.height);
+          stats.innerHTML += "height: " + finalheight + "m";
+          stats.innerHTML += "<br />"
+          data.weight = data.weight / 10;
+          let finalweight = checkAndAddDecimalZero(data.weight);
+          stats.innerHTML += "weight: " + finalweight + "kg"
+          stats.innerHTML += "<br />"
+          stats.innerHTML += "hp: " + data.stats[0].base_stat;
+          stats.innerHTML += "<br />"
+          stats.innerHTML += "attack: " + data.stats[1].base_stat;
+          stats.innerHTML += "<br />"
+          stats.innerHTML += "defense: " + data.stats[2].base_stat;
+          stats.innerHTML += "<br />"
+          stats.innerHTML += "special-attack: " + data.stats[3].base_stat;
+          stats.innerHTML += "<br />"
+          stats.innerHTML += "special-defense: " + data.stats[4].base_stat;
+          stats.innerHTML += "<br />"
+          "speed: " + data.stats[5].base_stat;
+
+
+        
+      })
+    
+
+  }); 
+
+
 
 function mod(a,b){
     if(a < 0){
@@ -24,9 +131,9 @@ function mod(a,b){
         return a;
     }
 
-}
+} 
 async function fetchPoke(current){
-    const apiUrl = `https://pokeapi.co/api/v2/type/${mod(current, 1017)}`;
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${mod(current, 1017)}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     return data;
@@ -51,12 +158,51 @@ rightarrow.addEventListener("click", () => {
           img.src = spriteUrl;
           img.alt = pokemonName;
           pokemon_name.textContent = data.name;
+          if(infoMode === true){
+            stats.innerHTML = '';
+            data.height /= 10
+            let finalheight = checkAndAddDecimalZero(data.height);
+            stats.innerHTML += "height: " + finalheight + "m";
+            stats.innerHTML += "<br />"
+            data.weight = data.weight / 10;
+            let finalweight = checkAndAddDecimalZero(data.weight);
+            stats.innerHTML += "weight: " + finalweight + "kg"
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "hp: " + data.stats[0].base_stat;
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "attack: " + data.stats[1].base_stat;
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "defense: " + data.stats[2].base_stat;
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "special-attack: " + data.stats[3].base_stat;
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "special-defense: " + data.stats[4].base_stat;
+            stats.innerHTML += "<br />"
+            "speed: " + data.stats[5].base_stat;
+          }else{
+            stats.innerHTML = '';
+            let y =0;
+            for(let i = 0; i < data.moves.length; i++){
+                stats.innerHTML += data.moves[i].move.name;
+                stats.innerHTML += "<br />"
+  
+                y++;
+                
+            }
+
+          }
           data.height /= 10
-          height.textContent = "height: " + data.height + "m";
+          let finalheight = checkAndAddDecimalZero(data.height);
+          height.textContent = "height: " + finalheight + "m";
           data.weight = data.weight / 10;
           let finalweight = checkAndAddDecimalZero(data.weight);
           weight.textContent = "weight: " + finalweight + "kg";
-         // hp.textContent = "hp: " + data.stats.stat.hp;
+          hp.textContent = "hp: " + data.stats[0].base_stat;
+          attack.textContent = "attack: " + data.stats[1].base_stat;
+          defense.textContent = "defense: " + data.stats[2].base_stat;
+          special_att.textContent = "special-attack: " + data.stats[3].base_stat;
+          special_def.textContent = "special-defense: " + data.stats[4].base_stat;
+          speed.textContent = "speed: " + data.stats[5].base_stat;
 
           
         } else {
@@ -97,6 +243,52 @@ rightarrow.addEventListener("click", () => {
           img.src = spriteUrl;
           img.alt = pokemonName;
           pokemon_name.textContent = data.name;
+          if(infoMode === true){
+              stats.innerHTML = '';
+            data.height /= 10
+            let finalheight = checkAndAddDecimalZero(data.height);
+            stats.innerHTML += "height: " + finalheight + "m";
+            stats.innerHTML += "<br />"
+            data.weight = data.weight / 10;
+            let finalweight = checkAndAddDecimalZero(data.weight);
+            stats.innerHTML += "weight: " + finalweight + "kg"
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "hp: " + data.stats[0].base_stat;
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "attack: " + data.stats[1].base_stat;
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "defense: " + data.stats[2].base_stat;
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "special-attack: " + data.stats[3].base_stat;
+            stats.innerHTML += "<br />"
+            stats.innerHTML += "special-defense: " + data.stats[4].base_stat;
+            stats.innerHTML += "<br />"
+            "speed: " + data.stats[5].base_stat;
+
+          }else{
+            stats.innerHTML = '';
+            let y =0;
+            for(let i = 0; i < data.moves.length; i++){
+                stats.innerHTML += data.moves[i].move.name;
+                stats.innerHTML += "<br />"
+  
+                y++;
+                
+            }
+
+          }
+          
+          data.height /= 10
+          height.textContent = "height: " + data.height + "m";
+          data.weight = data.weight / 10;
+          let finalweight = checkAndAddDecimalZero(data.weight);
+          weight.textContent = "weight: " + finalweight + "kg";
+          hp.textContent = "hp: " + data.stats[0].base_stat;
+          attack.textContent = "attack: " + data.stats[1].base_stat;
+          defense.textContent = "defense: " + data.stats[2].base_stat;
+          special_att.textContent = "special-attack: " + data.stats[3].base_stat;
+          special_def.textContent = "special-defense: " + data.stats[4].base_stat;
+          speed.textContent = "speed: " + data.stats[5].base_stat;
 
           
         } else {
@@ -116,6 +308,9 @@ rightarrow.addEventListener("click", () => {
    
 
   });
+
+  
+
 
   function checkAndAddDecimalZero(number) {
     // Convert the number to a string
@@ -140,6 +335,22 @@ rightarrow.addEventListener("click", () => {
 
 
 
+  async function getTotalMovesForPokemon(current) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${current}`);
+      
+      if (response.ok) {
+        const pokemonData = await response.json();
+        const totalMoves = pokemonData.moves.length;
+        console.log(`Total moves for ${current.name}: ${totalMoves}`);
+        return totalMoves;
+      } else {
+        console.error(`Failed to fetch data for ${current.name}.`);
+      }
+    } catch (error) {
+      console.error(`Error fetching data: ${error.message}`);
+    }
+  }
 
 
 
